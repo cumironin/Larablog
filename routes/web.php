99256af84d77e500ee2143a32225fc35224697f4
Muhammad Heri <cumironin@gmail.com>
2022-10-15 15:54:34 +0700

    fixing edit profile and show image to profile

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/', function () {
 Route::controller(AdminController::class)->group(function(){
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
     Route::get('/admin/profile', 'profile')->name('admin.profile');
+    Route::get('/edit/profile', 'editProfile')->name('edit.profile');
+    Route::post('/store/profile', 'storeProfile')->name('store.profile');
+
+
 
 });
 
